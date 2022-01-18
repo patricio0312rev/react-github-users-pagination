40bf1d3c856dea51b81fb58a09f15b0cb6409aa6
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-18 12:21:06 -0500

    Agregando funcionalidad para botones de next y prev

--- src/App.js
+++ src/App.js
@@ -18,6 +18,28 @@ function App() {
   const handlePage = (index) =>  {
     setPage(index);
   }
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if(prevPage < 0) {
+        prevPage = data.length - 1 ;
+      }
+
+      return prevPage;
+    })
+  }
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if(nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+
+      return nextPage;
+    })
+  }
   
   return (
     <main>
@@ -37,6 +59,9 @@ function App() {
 
         {
           !loading  && <div className='btn-container'>
+            <button className='prev-btn' onClick={prevPage}>
+              prev
+            </button>
             {
               data.map((item, index) => {
                 return (
@@ -46,6 +71,9 @@ function App() {
                 )
               })
             }
+            <button className='next-btn' onClick={nextPage}>
+              next
+            </button>
           </div>
         }
 
