c41f646a95b9d17556c6ba413ae22451887653b8
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-18 11:21:41 -0500

    Completando App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 
+import React from 'react';
+import { useFetch } from './hooks/useFetch';
+import Follower from './components/follower.component';
 
 function App() {
+  const { loading, data } = useFetch();
+  console.log(data);
   return (
-    <div className="App">
-      <h2>Hello world</h2>
-    </div>
+    <main>
+      <div className='section-title'>
+        <h1>{ loading ? 'Cargando...' : 'Pagination' }</h1>
+        <div className='underline'></div>
+      </div>
+
+      <section className='followers'>
+        <div className='container'>
+          {
+            data.map((follower) => {
+              return <Follower key={follower.id} {...follower} />
+            })
+          }
+        </div>
+      </section>
+    </main>
   );
 }
 
